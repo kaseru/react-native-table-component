5b6918eeea04d448cf9de8ba0ec81e86acaf5267
kaseru <kaseru@live.com>
2025-01-14 01:20:25 +0700

    Update rows.js

--- components/rows.js
+++ components/rows.js
@@ -1,47 +1,26 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
-import { Cell } from './cell';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { sum } from '../utils';
+import { Cell } from './cell';
 
 export class Row extends Component {
-  static propTypes = {
-    style: PropTypes.object,
-    textStyle: PropTypes.object
-  };
-
   render() {
-    const { data, style, widthArr, height, flexArr, textStyle, cellTextStyle, ...props } = this.props;
+    const { data, style, widthArr, height, flexArr, textStyle, onPress, activeOpacity = 0.8, ...props } = this.props;
     let width = widthArr ? sum(widthArr) : 0;
 
     return data ? (
-      <View style={[height && { height }, width && { width }, styles.row, style]}>
+      <TouchableOpacity activeOpacity={activeOpacity} onPress={onPress} disabled={!onPress} style={[height && { height }, width && { width }, styles.row, style]}>
         {data.map((item, i) => {
           const flex = flexArr && flexArr[i];
           const wth = widthArr && widthArr[i];
-          return (
-            <Cell
-              key={i}
-              data={item}
-              width={wth}
-              height={height}
-              flex={flex}
-              textStyle={[cellTextStyle && cellTextStyle(item), textStyle]}
-              {...props}
-            />
-          );
+          return <Cell key={i} data={item} width={wth} height={height} flex={flex} textStyle={textStyle} {...props} />;
         })}
-      </View>
+      </TouchableOpacity>
     ) : null;
   }
 }
 
 export class Rows extends Component {
-  static propTypes = {
-    style: PropTypes.object,
-    textStyle: PropTypes.object
-  };
-
   render() {
     const { data, style, widthArr, heightArr, flexArr, textStyle, ...props } = this.props;
     const flex = flexArr ? sum(flexArr) : 0;
@@ -51,18 +30,7 @@ export class Rows extends Component {
       <View style={[flex && { flex }, width && { width }]}>
         {data.map((item, i) => {
           const height = heightArr && heightArr[i];
-          return (
-            <Row
-              key={i}
-              data={item}
-              widthArr={widthArr}
-              height={height}
-              flexArr={flexArr}
-              style={style}
-              textStyle={textStyle}
-              {...props}
-            />
-          );
+          return <Row key={i} data={item} widthArr={widthArr} height={height} flexArr={flexArr} style={style} textStyle={textStyle} {...props} />;
         })}
       </View>
     ) : null;
